f1d3229f73f6a8a56f0c1a1453e063da63441b9f
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-10-13 13:55:16 +0700

    Initial commit

---
+++ app/src/main/java/com/myfirstcompose/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.myfirstcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.myfirstcompose.ui.theme.MyFirstComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Fajri")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("Benny")
+}
