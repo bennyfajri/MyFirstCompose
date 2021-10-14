67a7cc50636241bae36da9fe22c9e4d667df9cb3
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-10-14 09:25:40 +0700

    Row, column and basic sizing

--- app/src/main/java/com/myfirstcompose/MainActivity.kt
+++ app/src/main/java/com/myfirstcompose/MainActivity.kt
@@ -3,29 +3,46 @@ package com.myfirstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.myfirstcompose.ui.theme.MyFirstComposeTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Fajri")
+            //Row
+            Row(
+                modifier = Modifier
+                    .width(300.dp)
+                    .fillMaxHeight(0.7f)
+                    .background(Color.Green),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Hello")
+                Text("World")
+                Text("!!")
+            }
+
+            //Column
+//            Column(
+//                modifier = Modifier
+//                    .width(300.dp)
+//                    .fillMaxHeight(0.7f)
+//                    .background(Color.Green),
+//                verticalArrangement = Arrangement.SpaceAround,
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            ) {
+//                Text("Hello")
+//                Text("World")
+//                Text("!!")
+//            }
+
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Benny")
 }
