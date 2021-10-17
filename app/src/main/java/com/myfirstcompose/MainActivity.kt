09c992826aaa023b05e3f97a1da538eebbf8fbfe
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-10-17 22:16:05 +0700

    creating an image card composable

--- app/src/main/java/com/myfirstcompose/MainActivity.kt
+++ app/src/main/java/com/myfirstcompose/MainActivity.kt
@@ -3,41 +3,88 @@ package com.myfirstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            val painter = painterResource(id = R.drawable.kiana)
+            val description = "Kiana Herrscher of Flamescion"
+            val title = "Kiana Herrscher of Flamescion"
+            Box(
                 modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    .border(5.dp, Color.Magenta)
                     .padding(16.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(16.dp)
-                    .border(5.dp, Color.Red)
-                    .padding(10.dp)
-//                    .width(300.dp)
-//                    .requiredWidth(600.dp)
             ){
-                Text("Hello", modifier = Modifier.clickable {
-
-                })
-                Spacer(modifier = Modifier.height(50.dp))
-                Text("World")
+                ImageCard(painter = painter, contentDescription = description, title = title)
             }
+        }
+    }
+}
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    style = androidx.compose.ui.text.TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                )
+            }
         }
     }
 }
