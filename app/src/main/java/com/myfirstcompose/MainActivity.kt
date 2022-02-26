bd926e816424f2fc1b18d672b314cf9b5bd3b4cf
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-02-26 21:44:27 +0700

    animation on jetpack compose

--- app/src/main/java/com/myfirstcompose/MainActivity.kt
+++ app/src/main/java/com/myfirstcompose/MainActivity.kt
@@ -2,74 +2,65 @@ package com.myfirstcompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.OnBackPressedCallback
-import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         setContent {
-            val scaffoldState = rememberScaffoldState()
-             val scope = rememberCoroutineScope()
-           Scaffold(scaffoldState = scaffoldState) {
-               var counter by remember {
-                   mutableStateOf(0)
-               }
-               if(counter % 5 == 0 && counter > 0) {
-                   LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
-                       scaffoldState.snackbarHostState.showSnackbar("Hello")
-                   }
-               }
-               Button(onClick = { counter++ }) {
-                   Text(text = "Click me: $counter")
-               }
-           }
+        setContent {
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(
+                targetValue = sizeState,
+//                keyframes {
+//                    durationMillis = 5000
+//                    sizeState at 0 with LinearEasing
+//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+//                    sizeState * 2f at 5000
+//                }
+//                spring(
+//                    Spring.DampingRatioHighBouncy
+//                )
+                tween(
+                    durationMillis = 1000
+                )
+            )
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(durationMillis = 2000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+            Box(modifier = Modifier
+                .size(size)
+                .background(color),
+            contentAlignment = Alignment.Center) {
+                Button(onClick ={
+                    sizeState += 50.dp
+                }) {
+                    Text(text = "Increase Size")
+                }
+            }
         }
     }
 
 }
-
-//var i = 0
-//
-//@Composable
-//fun myComposable(backPressedDispatcher: OnBackPressedDispatcher) {
-//
-//    val callback = remember {
-//        object : OnBackPressedCallback(true) {
-//            override fun handleOnBackPressed() {
-//                // Do something
-//            }
-//        }
-//    }
-//
-//    DisposableEffect(key1 = backPressedDispatcher) {
-//        backPressedDispatcher.addCallback(callback)
-//        onDispose {
-//            callback.remove()
-//        }
-//    }
-//    Button(onClick = { /*TODO*/ }) {
-//        Text(text = "Click me")
-//    }
-//}
